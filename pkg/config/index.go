9fe3727332cb957a68924c70abed43fcfc56b883
isaiahiroko <isaiahiroko@gmail.com>
2024-10-02 07:13:27 +0100

    improve config set

--- pkg/config/index.go
+++ pkg/config/index.go
@@ -3,27 +3,22 @@ package config
 import (
 	"log"
 
-	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
 
 var cf any
 
-func Set[T any](environment string) *T {
-	if environment == "" {
-		environment = gin.DebugMode
-	}
-
-	viper.AddConfigPath("env")
-	viper.SetConfigName(environment)
+func Set[T any]() *T {
+	viper.SetConfigFile(".env")
 	viper.SetConfigType("env")
+	viper.AddConfigPath(".")
 
 	// load config from env
 	viper.AutomaticEnv()
 
 	// Viper reads all the variables from env file and log error if any found
 	if err := viper.ReadInConfig(); err != nil {
-		log.Fatal("Error reading env file", err)
+		log.Println("Error reading env file", err)
 	}
 
 	// Viper unmarshal the loaded env varialbes into the struct
